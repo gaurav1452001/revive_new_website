fc2fc4f8e4974f7d906607d82f53c8d9b6072893
Gaurav Kumar <gaurav1452001@gmail.com>
2025-08-22 19:06:22 +0530

    build error fixes

--- src/components/ui/animated-tooltip.tsx
+++ src/components/ui/animated-tooltip.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import React, { useState, useRef } from "react";
+import React, {useRef } from "react";
 import {
-    useTransform,
     useMotionValue,
-    useSpring,
 } from "motion/react";
 
 export const AnimatedTooltip = ({
@@ -17,19 +15,8 @@ export const AnimatedTooltip = ({
         image: string;
     }[];
 }) => {
-    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
-    const springConfig = { stiffness: 100, damping: 15 };
     const x = useMotionValue(0);
     const animationFrameRef = useRef<number | null>(null);
-
-    const rotate = useSpring(
-        useTransform(x, [-100, 100], [-45, 45]),
-        springConfig,
-    );
-    const translateX = useSpring(
-        useTransform(x, [-100, 100], [-50, 50]),
-        springConfig,
-    );
 
     const handleMouseMove = (event: any) => {
         if (animationFrameRef.current) {
@@ -48,8 +35,6 @@ export const AnimatedTooltip = ({
                 <div
                     className="group relative -mr-4"
                     key={item.name}
-                    onMouseEnter={() => setHoveredIndex(item.id)}
-                    onMouseLeave={() => setHoveredIndex(null)}
                 >
                     <img
                         onMouseMove={handleMouseMove}
